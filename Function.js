864d9707120b8cb9747091b6a6cd7495116ebda0
DINESH BAMNIA <mpkhadirajgarh@gmail.com>
2025-09-18 11:09:36 +0530

    done

---
+++ Function.js
@@ -0,0 +1,25 @@
+// Primitive data type :  String , Number, Boolean, Null , Undefined , Symbol , Bigint
+
+// Object , Arry 
+
+let num1 = 50;
+let num2 = 61;
+
+function AddTwoNumber() {
+    return num1 + num2
+}
+
+let result = AddTwoNumber(); // function call krna 
+
+console.log(result);
+
+
+// function with parameter 
+
+function SumOf(n1,n2){
+return n1 + n2
+}
+
+let res = SumOf(125,126); // parameter pass krna 
+
+console.log(res);

e0c3f1ba9fa76cf8f9301ab42e2422eb0f901727
DINESH BAMNIA <mpkhadirajgarh@gmail.com>
2025-09-26 11:42:42 +0530

    Add files via upload

---
+++ and.js
@@ -0,0 +1,16 @@
+// gen z Born year 1997 to 2012 
+// gen alpha born year 2010 to 2025 
+
+let DateofBirthYear = 1995;
+
+if(DateofBirthYear >=1997 && DateofBirthYear <=2012) {
+
+    console.log(`You are Gen Z Because Your Birth day year: ${DateofBirthYear}`)
+}
+else if(DateofBirthYear >=2010 && DateofBirthYear <=2025) {
+    console.log(`You are Gen Alpha, because your age year: ${DateofBirthYear}`)
+}
+
+else {
+    console.log(`You can Family Person`)
+}
---
+++ not.js
@@ -0,0 +1,9 @@
+let age = 20; 
+
+if (!age > 19) {
+
+    console.log(`You are Teenagar`)
+} 
+else {
+    console.log(`You are not Teenager`)
+}
